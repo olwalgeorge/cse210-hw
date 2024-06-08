e94313bc9a425dc2e9f54cca6016df428a3c35b9
George Olwal <olwalgeorge@live.com>
2024-06-08 17:06:24 +0300

    Prep 3 Core 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,17 +4,17 @@ using System;
 /// <name>George Olwal</name>
 /// </author>
 /// <summary>
-/// Prep 3 Core 2
-/// This is a number guessing game where user is asked to guess a number and the 
-/// program will continously tell them if their guess is higher or lower than the magic number untill the guess is right
+/// Prep 3 Core 3
+/// Generate random number between 1 to 100 and ask user to guess it
+/// program will continously tell if the guess is higher or lower than the magic number untill the guess is right
 /// </summary>
 class Program
 {
     static void Main(string[] args)
     {   
-        // Ask the user for magic number and store in a viariable
-        Console.Write("What is the magic number?");
-        int magicNumber = int.Parse(Console.ReadLine());
+        // Generate random number between 1 and 100 as the magic number
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
 
         // Initialize guess
         int guess = 0;
