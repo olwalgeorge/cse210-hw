88a48b4d6736a9ae141652cd81cf7bb73c979e8a
George Olwal <olwalgeorge@live.com>
2024-06-08 14:57:26 +0300

    Prep 2  Core 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,44 @@
 using System;
 
+/// <author>
+/// <name>George Olwal</name>
+/// </author>
+/// <summary>
+/// Prep 2  Core 1
+/// This program will allow the user to input their grade percentage and output
+/// their letter grade. 
+/// </summary>
+
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Get student's percentage
+        Console.Write("What is your grade percentage? ");
+        int studentMarks = int.Parse(Console.ReadLine()); 
+
+        // Get the student grade and print accordingly       
+        if(studentMarks >= 90)
+            {
+                Console.WriteLine("A");
+            }
+        else if(studentMarks >= 80)
+            {
+                Console.WriteLine("B");
+            }
+        else if(studentMarks >= 70)
+            {
+                Console.WriteLine("C");
+            }
+        else if(studentMarks >= 60)
+            {
+                Console.WriteLine("D");
+            }
+        else
+            {
+                Console.WriteLine("F");
+            }
     }
-}
+
+}            
