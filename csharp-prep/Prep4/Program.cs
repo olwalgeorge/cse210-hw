45e6db2024cc3c7de2581d00efd685d846198274
George Olwal <olwalgeorge@live.com>
2024-06-08 17:46:45 +0300

    C# Prep 4 Core 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,21 +3,22 @@ using System;
 /// George Olwal
 /// </Author>
 /// <Summary>
-/// C# Prep 4 Core
+/// C# Prep 4 Core 1
 /// This program will ask the user to enter a list of numbers from while inserting on the list until 0 is entered
-/// the program then displays the list on the console
+/// the program then displays the list on the console,
+/// calculates and display sum
 /// </Summary>
 
 class Program
 {
     static void Main(string[] args)
     {
-
-        int num;
+        // Ask user to enter numbers and append them to a list
+        int num;        
         List<int> numbers = new List<int>();
         do
             {
-                Console.Write("Enter a list of numbers, type 0 when finished.");
+                Console.Write("Enter a list of numbers, type 0 when finished. ");
                 num = int.Parse(Console.ReadLine());
 
                 if (num != 0)
@@ -30,5 +31,13 @@ class Program
         {
             Console.WriteLine(number);
         }
+
+        // Calculate sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
