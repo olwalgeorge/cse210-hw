d178a2132cdfd9f1dad24131057db7ec601b749b
George Olwal <olwalgeorge@live.com>
2024-06-08 18:00:21 +0300

    C# Prep 4 Core 3

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,10 +3,10 @@ using System;
 /// George Olwal
 /// </Author>
 /// <Summary>
-/// C# Prep 4 Core 2
+/// C# Prep 4 Core 3
 /// This program will ask the user to enter a list of numbers from while inserting on the list until 0 is entered
 /// the program then displays the list on the console,
-/// calculates and display sum and average
+/// calculates and display sum, average and largest number
 /// </Summary>
 
 class Program
@@ -32,16 +32,26 @@ class Program
             Console.WriteLine(number);
         }
 
-        // Calculate sum
+        // Calculate sum & largest number
         int sum = 0;
+        double largest = double.NegativeInfinity;   
         foreach (int number in numbers)
         {
             sum += number;
+
+            if (number > largest)
+            {
+                largest = number;
+            }
+
         }
         Console.WriteLine($"The sum is: {sum}");
 
         //Calculate average
         double average = (double)sum / numbers.Count;
         Console.WriteLine($"The average is: {average}");
+
+        //Display largest number
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
