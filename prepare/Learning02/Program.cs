960d022bd56777a266a1df54709e83a086a1c35f
George Olwal <olwalgeorge@live.com>
2024-06-09 18:08:00 +0300

    Add a Display method to the Resume class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -10,6 +10,8 @@ using System;
 /// Create 2 job instances and display jobs details using method calls
 /// Create resume class
 /// Test Resume class
+/// Add a Display method to the Resume class
+/// Display resume details using method call 
 /// </summary>
 
 class Program
@@ -28,11 +30,7 @@ class Program
         job2._company = "Etec Communications";
         job2._jobTitle = "Software Engineer";
         job2._startYear = 2022;
-        job2._endYear = 2024;
-
-        //Display jobs details using method calls
-        job1.DisplayJobDetails();
-        job2.DisplayJobDetails();
+        job2._endYear = 2024;        
 
         //Create resume instance
         Resume myResume = new Resume();
@@ -40,10 +38,8 @@ class Program
         myResume._jobList.Add(job1);
         myResume._jobList.Add(job2);
         
-        //Display resume job1 title using dot notation
-        Console.WriteLine(myResume._name);
-        Console.WriteLine(myResume._jobList[0]._jobTitle);     
-        
+        //Display resume details using method call
+        myResume.DisplayResumeDetails();        
 
     }
 }
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -4,6 +4,8 @@
 /// <summary>
 /// Abstraction Learning Activity
 /// Create resume class
+/// Add a Display method to the Resume class
+/// Display resume details using method call
 /// </summary>
 
 public class Resume
@@ -11,6 +13,19 @@ public class Resume
     //Fields
     public string _name;
     public List<Job> _jobList = [];    
+
+    //Display resume details
+    public void DisplayResumeDetails()
+    {
+        //Display name
+        Console.WriteLine(_name);
+
+        //Display jobs details by iterative call to job Display method
+        foreach (var item in _jobList)
+        {
+            item.DisplayJobDetails();
+        };
+    }
     
 }
 
