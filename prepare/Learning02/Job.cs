d239f7ed43b8ad51ad8fdba8e81928b5d3283ac5
George Olwal <olwalgeorge@live.com>
2024-06-09 16:23:49 +0300

    Create the Job class

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,19 @@
+using System;
+
+/// <author>
+/// <name>George Olwal</name>
+/// </author>
+/// <summary>
+/// Abstraction Learning Activity
+/// Create the Job class
+/// </summary>
+
+
+public class Job
+{
+    //Fields
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+}
