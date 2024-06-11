f5aa55f11e638dd14444df00cc80b6fce087d91c
George Olwal <olwalgeorge@live.com>
2024-06-11 14:39:18 +0300

    Create the Constructors

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -5,14 +5,40 @@ using System;
 /// <summary>
 /// W03 Prepare: Learning Activity
 /// Create a Fraction class
+/// Create the Constructors
 /// </summary>
 
 
 
 public class Fraction
 {   
+    //Fields (private)
     private int _top;
     private int _bottom;
+
+    // Constructor - initialize with(1/1)
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;  
+        Console.WriteLine("Constructor called with default values"); // For verification at this stage      
+    }
+
+    //Constructor - sets whole nuumber (numerator/1)
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+        Console.WriteLine($"Constructor called with whole number, {_top}"); //For verication only
+    }
+
+    //Constructor - sets fraction (numerator/denominator)
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+        Console.WriteLine($"Constructor called with numerator, {_top} and denominator, {_bottom}");// for verification
+    }
 
 }
 
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,21 @@
 using System;
 
+/// <author>
+/// <name>George Olwal</name>
+/// </author>
+/// <summary>
+/// W03 Prepare: Learning Activity
+/// Create a Fraction class
+/// Create the Constructors
+/// </summary>
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fract1 = new Fraction(); // instantiate with default values (1/1)
+        Fraction fract2 = new Fraction(6); // instantiate with whole number
+        Fraction fract3 = new Fraction(6, 7); // instantiate with fraction (6/7)
+        
     }
 }
