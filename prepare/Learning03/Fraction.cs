2064b15c74f028edb6ac82c49e4748e8ee393b4b
George Olwal <olwalgeorge@live.com>
2024-06-11 13:55:28 +0300

    Create a Fraction class

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,23 @@
+using System;
+/// <author>
+/// <name>George Olwal</name>
+/// </author>
+/// <summary>
+/// W03 Prepare: Learning Activity
+/// Create a Fraction class
+/// </summary>
+
+
+
+public class Fraction
+{   
+    private int _top;
+    private int _bottom;
+
+}
+
+
+
+
+
+
